04d30c578dde7395c8d5c72755d521b53607f46b
Igor Ferreira Junqueira Portugal <139009828+KingZumbu@users.noreply.github.com>
2025-08-26 22:05:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,372 @@
+const p1 = "119,90"
+const p2 = "139,90"
+const p3 = "69,90"
+const p4 = "79,90"
+const p5 = "39,90"
+const meuCelular = "5525993106425"
+
+const perfumes={
+    "1":{"nome":"Kayak",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft1.jpg",
+    },"2":{"nome":"Bvlgari",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft2.jpg",
+    },"3":{"nome":"Scandal Homme ",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft3.jpg",
+    },"4":{"nome":"Hugo Boss",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft4.jpg",
+    },"5":{"nome":"Polo Black",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft5.jpg",
+    },"6":{"nome":"One Million",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft6.jpg",
+    },"7":{"nome":"Essencial ",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft7.jpg",
+    },"8":{"nome":"Calvin Klein",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft8.jpg",
+    },"9":{"nome":"Drakar Noir",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft9.jpg",
+    },"10":{"nome":"Diesel Fuel",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft10.jpg",
+    },"11":{"nome":"Cool Water",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft11.jpg",
+    },"12":{"nome":"Ferrari Black",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft12.jpg",
+    },"13":{"nome":"Paco Rabanne",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft13.jpg",
+    },"14":{"nome":"212 - Sexy, Heroes, Vip",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft14.jpg",
+    },"15":{"nome":"Animale",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft15.jpg",
+    },"16":{"nome":"My Way",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/sem foto.jpg",
+    },"17":{"nome":"Dolce Gabbana",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft17.jpg",
+    },"18":{"nome":"Armani",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft18.jpg",
+    },"19":{"nome":"Azarro",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft19.jpg",
+    },"20":{"nome":"Mont Blanc",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft20.jpg",
+    },"21":{"nome":"Conexion",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft21.jpg",
+    },"22":{"nome":"Silver Scent",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft22.jpg",
+    },"23":{"nome":"Insensatez",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft23.jpg",
+    },"24":{"nome":"Bad Boy",
+        "sexo":"masculino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft24.jpg",
+    },"25":{"nome":"Invictus",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft25.jpg",
+    },"26":{"nome":"Creed Aventus",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft26.jpg",
+    },"27":{"nome":"Bvlgari Black",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft27.jpg",
+    },"28":{"nome":"Bacarah",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft28.jpg",
+    },"29":{"nome":"Sauvage",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft29.jpg",
+    },"30":{"nome":"Malbec Noir",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft30.jpg",
+    },"31":{"nome":"Bleu Chanel",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft31.jpg",
+    },"32":{"nome":"Jean Paul Galtier",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft32.jpg",
+    },"33":{"nome":"Lancaster",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft33.jpg",
+    },"34":{"nome":"Mandarino",
+        "sexo":"masculino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft34.jpg",
+    },"35":{"nome":"Scandal",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft35.jpg",
+    },"36":{"nome":"Angel",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft36.jpg",
+    },"37":{"nome":"Chloe",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft37.jpg",
+    },"38":{"nome":"Egeo Dolce",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft38.jpg",
+    },"39":{"nome":"Olympea",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft39.jpg",
+    },"40":{"nome":"Florata Blue",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft40.jpg",
+    },"41":{"nome":"CK One",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft41.jpg",
+    },"42":{"nome":"Hypnotic Poison",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft42.jpg",
+    },"43":{"nome":"Chanel 5",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft43.jpg",
+    },"44":{"nome":"Absinto",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft44.jpg",
+    },"45":{"nome":"Opium",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft45.jpg",
+    },"46":{"nome":"Lili",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/sem foto.jpg",
+    },"47":{"nome":"Jadore",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft47.jpg",
+    },"48":{"nome":"212 - Sexy, Heroes, Vip",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft48.jpg",
+    },"49":{"nome":"Bvlgari",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft49.jpg",
+    },"50":{"nome":"Amor Amor",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft50.jpg",
+    },"51":{"nome":"Dolce Gabbana",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft51.jpg",
+    },"52":{"nome":"Armani",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft52.jpg",
+    },"53":{"nome":"Lou Lou",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft53.jpg",
+    },"54":{"nome":"Flower by Kenzo",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft54.jpg",
+    },"55":{"nome":"La Vie est Belle",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft55.jpg",
+    },"56":{"nome":"Gabriela Sabatine",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft56.jpg",
+    },"57":{"nome":"Euphoria",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft57.jpg",
+    },"58":{"nome":"Coco Mademoiselle",
+        "sexo":"feminino",
+        "tipo":"normal",
+        "foto":"/perfumesDiagrama/ft58.jpg",
+    },"59":{"nome":"Dior Joy",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft59.jpg",
+    },"60":{"nome":"Scandal by Night",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/sem foto.jpg",
+    },"61":{"nome":"Good Girl CH",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft61.jpg",
+    },"62":{"nome":"Haya Lattafa",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft62.jpg",
+    },"63":{"nome":"Ameerat al Arab Rose",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft63.jpg",
+    },"64":{"nome":"Vezzo",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft64.jpg",
+    },"65":{"nome":"Al Wataniah Shagaf",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft65.jpg",
+    },"66":{"nome":"Jo Malone",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft66.jpg",
+    },"67":{"nome":"Pegasus de Marly",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft67.jpg",
+    },"68":{"nome":"Miss Dior",
+        "sexo":"feminino",
+        "tipo":"especial",
+        "foto":"/perfumesDiagrama/ft68.jpg",
+    },
+
+}
+
+function sortearNumeros() {
+  let numeros = new Set();
+
+  while (numeros.size < 10) {
+    let n = Math.floor(Math.random() * 68) + 1; // gera de 1 a 68
+    numeros.add(n);
+  }
+
+  let arr = Array.from(numeros);
+  return arr
+}
+
+
+function motorDeEscolha(){
+        let arr = sortearNumeros()
+
+        let produto1inicial = arr[1]
+        let produto2inicial = arr[2]
+        let produto3inicial = arr[3]
+        let produto4inicial = arr[4]
+        let produto5inicial = arr[5]
+        let produto6inicial = arr[6]
+        let produto7inicial = arr[7]
+        let produto8inicial = arr[8]
+        let produto9inicial = arr[9]
+        let produto0inicial = arr[0]
+
+        let prodt1 = document.getElementById("aprt1")
+        prodt1.style.backgroundImage = `url(${perfumes[produto1inicial]["foto"]})`;
+        prodt1.style.backgroundSize = "cover";   // cobre toda a div
+        prodt1.style.backgroundPosition = "center"; // centraliza
+
+        let prodt2 = document.getElementById("aprt2")
+        prodt2.style.backgroundImage = `url(${perfumes[produto2inicial]["foto"]})`;
+        prodt2.style.backgroundSize = "cover";   // cobre toda a div
+        prodt2.style.backgroundPosition = "center"; // centraliza
+
+        let prodt3 = document.getElementById("aprt3")
+        prodt3.style.backgroundImage = `url(${perfumes[produto3inicial]["foto"]})`;
+        prodt3.style.backgroundSize = "cover";   // cobre toda a div
+        prodt3.style.backgroundPosition = "center"; // centraliza
+
+        let prodt4 = document.getElementById("aprt4")
+        prodt4.style.backgroundImage = `url(${perfumes[produto4inicial]["foto"]})`;
+        prodt4.style.backgroundSize = "cover";   // cobre toda a div
+        prodt4.style.backgroundPosition = "center"; // centraliza
+
+        let prodt5 = document.getElementById("aprt5")
+        prodt5.style.backgroundImage = `url(${perfumes[produto5inicial]["foto"]})`;
+        prodt5.style.backgroundSize = "cover";   // cobre toda a div
+        prodt5.style.backgroundPosition = "center"; // centraliza
+
+        let prodt6 = document.getElementById("aprt6")
+        prodt6.style.backgroundImage = `url(${perfumes[produto6inicial]["foto"]})`;
+        prodt6.style.backgroundSize = "cover";   // cobre toda a div
+        prodt6.style.backgroundPosition = "center"; // centraliza
+
+        let prodt7 = document.getElementById("aprt7")
+        prodt7.style.backgroundImage = `url(${perfumes[produto7inicial]["foto"]})`;
+        prodt7.style.backgroundSize = "cover";   // cobre toda a div
+        prodt7.style.backgroundPosition = "center"; // centraliza
+
+        let prodt8 = document.getElementById("aprt8")
+        prodt8.style.backgroundImage = `url(${perfumes[produto8inicial]["foto"]})`;
+        prodt8.style.backgroundSize = "cover";   // cobre toda a div
+        prodt8.style.backgroundPosition = "center"; // centraliza
+
+        let prodt9 = document.getElementById("aprt9")
+        prodt9.style.backgroundImage = `url(${perfumes[produto9inicial]["foto"]})`;
+        prodt9.style.backgroundSize = "cover";   // cobre toda a div
+        prodt9.style.backgroundPosition = "center"; // centraliza
+
+        let prodt0 = document.getElementById("aprt0")
+        prodt0.style.backgroundImage = `url(${perfumes[produto0inicial]["foto"]})`;
+        prodt0.style.backgroundSize = "cover";   // cobre toda a div
+        prodt0.style.backgroundPosition = "center"; // centraliza
+}
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   motorDeEscolha()
+});
